5ed36981808444b5efd3bacb94e48a5b553bcb51
Nguyen Huynh Chi <huynhchi0904@gmail.com>
2025-03-27 14:49:13 +0700

    test commit in intelliJ

---
+++ Spring-boot/test/target/generated-sources/annotations/com/first_project/test/mapper/UserMapperImpl.java
@@ -0,0 +1,64 @@
+package com.first_project.test.mapper;
+
+import com.first_project.test.dto.request.UserCreationRequest;
+import com.first_project.test.dto.request.UserUpdateRequest;
+import com.first_project.test.dto.response.UserResponse;
+import com.first_project.test.entity.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toUser(UserCreationRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setUsername( request.getUsername() );
+        user.setPassword( request.getPassword() );
+        user.setFirstName( request.getFirstName() );
+        user.setLastName( request.getLastName() );
+        user.setDob( request.getDob() );
+
+        return user;
+    }
+
+    @Override
+    public UserResponse toUserResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponse.UserResponseBuilder userResponse = UserResponse.builder();
+
+        userResponse.id( user.getId() );
+        userResponse.username( user.getUsername() );
+        userResponse.password( user.getPassword() );
+        userResponse.firstName( user.getFirstName() );
+        userResponse.lastName( user.getLastName() );
+        userResponse.dob( user.getDob() );
+
+        return userResponse.build();
+    }
+
+    @Override
+    public void updateUser(User user, UserUpdateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        user.setUsername( request.getUsername() );
+        user.setPassword( request.getPassword() );
+        user.setFirstName( request.getFirstName() );
+        user.setLastName( request.getLastName() );
+        user.setDob( request.getDob() );
+    }
+}
